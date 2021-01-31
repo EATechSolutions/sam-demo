6d5f05393551436f227d581dedbd8b1f2cf6d232
Elroy Antao <elroyantao@gmail.com>
2021-01-31 21:35:03 +0000

    remove ses

--- public-methods/send.js
+++ public-methods/send.js
@@ -1,12 +1,12 @@
-const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses")
+// const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses")
 const  processResponse = require('./processResponse.js');
 const UTF8CHARSET = 'UTF-8';
 
 const CLIENT_EMAIL = process.env.CLIENT_EMAIL
 const FROM_EMAIL = process.env.FROM_EMAIL
-const REGION = process.env.REGION
+// const REGION = process.env.REGION
 
-const ses = new SESClient({ region: REGION });
+// const ses = new SESClient({ region: REGION });
 
 exports.handler = async event => {
   if (event.httpMethod === 'OPTIONS') {
@@ -58,8 +58,8 @@ exports.handler = async event => {
   };
 
   try {
-    await ses.send(new SendEmailCommand(emailParams));
-    return processResponse(true);
+    // await ses.send(new SendEmailCommand(emailParams));
+    return processResponse(true, emailParams);
   } catch (err) {
     console.error(err, err.stack);
     const errorResponse = `Error: Execution update, caused a SES error, please look at your logs.`;
